91c3b82fc519454f323ab5106f74190830bc2b42
jchen463 <jchen463@gmail.com>
2018-01-20 17:53:31 -0800

    labels

---
+++ labels.py
@@ -0,0 +1,33 @@
+import numpy as np
+import csv
+np.random.seed(42)
+codex = [['pets', 'no pets'],
+		 ['stay in and watch netflix', 'go out instead of staying in'],
+		 ['sexual compatibility matters', 'sexual compatibility does not matter'],
+		 ['cup is half-empty','cup is half-full'],
+		 ['morning person','night person'],
+		 ['city person','country person'],
+		 ['clean','messy'],
+		 ['religious','not religious'],
+		 ['want children','do not want children'],
+		 ['eat in','eat out'],
+		 ['play sports','do not play sports'],
+		 ['follows politics','does not follow politics'],
+		 ['spontaneous','predictable'],
+		 ['want long-term relationship','does not want long-term relationship'],
+		 ['procrastinates','does not procrastinate'],
+		 ['likes to travel','does not like to travel'],
+		 ['introvert','extrovert'],
+		 ['close to your family','not close to your family'],		 
+		 ]
+
+train_size = 400
+codex_length = len(codex)
+ans = np.random.randint(2, size=(train_size,codex_length))
+profiles = [[codex[i][ans[j][i]] for i in range(codex_length)] for j in range(train_size)]
+labels = [0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0]
+labeled_data = [[labels[i], ans[i]] for i in range(train_size)]
+
+with open("labels.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(labeled_data)
