da2230c570e392a35fa71131fbb9a298a9769bcd
jchen463 <jchen463@gmail.com>
2018-01-20 20:39:20 -0800

    include survey as property of class and into makeUser

--- routes.py
+++ routes.py
@@ -8,6 +8,7 @@ BASE_URL = os.path.abspath(os.path.dirname(__file__))
 CLIENT_APP_FOLDER = os.path.join(BASE_URL, "dist")
 
 import numpy as np
+import datetime
 np.random.seed(42)
 codex = [['pets', 'no pets'],
 		 ['stay in and watch netflix', 'go out instead of staying in'],
@@ -36,7 +37,7 @@ profiles = [[codex[i][ans[j][i]] for i in range(codex_length)] for j in range(tr
 labels = [None] * train_size
 
 class User:
-	def __init__(self, name, description, picture, interests, likes=[], likedBy=[], matches=[]):
+	def __init__(self, name, description, picture, interests, likes=[], likedBy=[], matches=[], survey=[]):
 		self.name = name
 		self.description = description
 		self.picture = picture
@@ -44,6 +45,7 @@ class User:
 		self.likes = likes
 		self.liked = likedBy
 		self.matches = matches
+		self.survey = survey
 	def like(userName):
 		liked.append(userName)
 		if userName in likedBy:
@@ -64,19 +66,19 @@ auth = {'wellford': ['wellford1', 'Wellford Chan'],
 users = {'Wellford Chan': User(name='Wellford Chan',
 			description='the boi',
 			picture='https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/11061206_10202668311107436_8620547020144133577_n.jpg?oh=9e13008c4ca1bbbd6e221369f8776d41&oe=5ADC8F1A',
-			interests='long walks on the beach' ),
+			interests='long walks on the beach', survey=[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0] ),
 		'James Chen': User(name='James Chen',
 			description='kassidin main',
 			picture='https://scontent.fsnc1-1.fna.fbcdn.net/v/l/t1.0-9/14079948_10208635955750119_4515098002705318896_n.jpg?oh=659971a58f78a90e1b3facefec0b515f&oe=5AE97E85',
-			interests='long walks on the beach' ),
+			interests='long walks on the beach', survey=[0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0] ),
 		'Kushtie Nguyen': User(name='Kushtie Nguyen',
 			description='smokes joints',
 			picture='https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/11146244_955466264494134_1735259667299678513_n.jpg?oh=5ec018932da6d589edb03bc1790b8322&oe=5AD83BE4',
-			interests='long walks on the beach' ),
+			interests='long walks on the beach', survey=[0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0] ),
 		'Ashwinee Panda': User(name='Ashwinee Panda',
 			description='no longer endangered',
 			picture='https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-9/26814904_878256455667619_4477007155290854499_n.jpg?oh=005ebf3cd5721f7d675865bfa307729a&oe=5AE4C9C2',
-			interests='long walks on the beach' )
+			interests='long walks on the beach', survey=[0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1] )
 		}
 
 def myconvert(o):
@@ -108,6 +110,7 @@ def getLabels():
 @application.route('/api/makeUser/', methods=["POST"])
 def makeUser():
 	r = request.get_json()
+	print(r)
 	if "userName" not in r:
 		return json.dumps("bamboozle")
 	auth[r['userName']] = (r['password'], r['name'])
@@ -115,7 +118,8 @@ def makeUser():
 		r['name'],
 		r['description'],
 		r['picture'],
-		r['interests']
+		r['interests'],
+		survey=r['survey']
 		)
 	users[r['name']] = user
 	return json.dumps(user, default = myconvert)
